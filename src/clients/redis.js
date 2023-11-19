dbd0a994966a427198f664bf2d108c268317127d
Muhammad M <muhammadmp1997@gmail.com>
2023-11-20 01:24:52 +0330

    project dockerized

--- src/clients/redis.js
+++ src/clients/redis.js
@@ -1,7 +1,9 @@
 import { createClient } from 'redis'
 
-const redis = await createClient()
-  .on('error', err => console.log('Redis Client Error', err))
+const redis = await createClient({ socket: { host: 'redis' } })
+  .on('error', err => {
+    console.error('Redis Client Error', err)
+  })
   .connect()
 
 export default redis
